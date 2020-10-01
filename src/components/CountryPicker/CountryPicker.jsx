4b768a4f114a20d0f255a3815f2f426bb0c1cc04
Nitin Ranganath <nitinranganath@gmail.com>
2020-10-01 19:02:59 +0530

    Implement dark mode toggle functionality

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,29 +1,34 @@
-import React, {useState, useEffect} from 'react';
-import {NativeSelect, FormControl, StylesProvider} from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import cx from "classnames";
+import styles from "./CountryPicker.module.css";
+import { NativeSelect, FormControl, StylesProvider } from "@material-ui/core";
 
-import {fetchcountries} from '../../api';
-const CountryPicker = ({handleCountryChange}) => {
+import { fetchcountries } from "../../api";
+const CountryPicker = ({ handleCountryChange }) => {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
 
-    const [fetchedCountries, setFetchedCountries] = useState([]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedCountries(await fetchcountries());
+    };
+    fetchAPI();
+  }, [setFetchedCountries]);
 
-    useEffect(() => {
-        const fetchAPI = async () => {
-            setFetchedCountries(await fetchcountries());
-        }
-        fetchAPI();
-    }, [setFetchedCountries]);
-
-
-    return (
-        <FormControl className={StylesProvider.formControl}>
-            <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
-                <option value="">Global</option>
-                {fetchedCountries.map((country,i) => <option key={i} value={country}>{country}</option>)}
-            </NativeSelect>
-        </FormControl>
-    )
-}
-
-
+  return (
+    <FormControl className={cx(styles.formControl, StylesProvider.formControl)}>
+      <NativeSelect
+        defaultValue=""
+        onChange={(e) => handleCountryChange(e.target.value)}
+      >
+        <option value="">Global</option>
+        {fetchedCountries.map((country, i) => (
+          <option key={i} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
+};
 
 export default CountryPicker;
